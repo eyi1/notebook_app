c54fb9b69f53d6477ac1a662af8f3801c3874e51
github username <github email address>
2019-02-18 15:18:20 -0500

    set up routes and namespace to corresponding actions and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,22 @@
 Rails.application.routes.draw do
-  resources :notes
-  resources :users
-  #get 'notebooks/index'
-  #resources :notebooks
+
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resources :notes
+    end
+  end
 
   namespace :api do
     namespace :v1 do
       resources :notebooks 
     end
   end
+    #get 'notebooks/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
