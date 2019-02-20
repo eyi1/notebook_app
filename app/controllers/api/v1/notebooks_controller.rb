f148788e85611fdf60ad6e859bbb407bc2eefb5d
github username <github email address>
2019-02-20 13:32:43 -0500

    create a new Notebook with params (hard-coded user_id for now)

--- app/controllers/api/v1/notebooks_controller.rb
+++ app/controllers/api/v1/notebooks_controller.rb
@@ -1,12 +1,15 @@
 module Api::V1
   class NotebooksController < ApplicationController
-  
+    before_action :set_notebook, only: [:show, :update, :destroy]
+
     def index
     @notebooks = Notebook.all 
     render json: @notebooks
     end
 
     def create
+      #@notebook = current_user.notebooks.build(notebook_params)
+      @notebook = Notebook.new(name: params[:name], user_id: 1)
       if @notebook && @notebook.save
         render json: @notebook
       else
@@ -15,6 +18,7 @@ module Api::V1
     end
 
     def show
+      render json: @notebook
     end
 
     def update 
