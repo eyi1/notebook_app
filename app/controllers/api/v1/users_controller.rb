dfa8dd6025f4cc43fec44ab35fbe8ec5ed580c55
github username <github email address>
2019-02-20 20:49:35 -0500

    add controller actions and set user params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,27 +1,45 @@
 module Api::V1
     class UsersController < ApplicationController
-    
+      before_action :set_user, only: [:show, :update, :destroy]
+
       def index
       @users = User.all 
       render json: @users
       end
 
-      def new 
-      end
-
       def create
+        @user = User.new(user_params)
+        if @user.save
+          @user.create
+            render json: @user
+        else
+          render json: @user.errors, status: 400
+        end
       end
 
       def show
-      end
-
-      def edit
+        render json: @user
       end
 
       def update 
+        if @user.update(user_params)
+          render json: @user
+        else
+          render json: @user.errors, status: 400
+        end
       end
 
       def destroy
+        @user.destroy
+      end
+
+      private
+      def set_user
+        @user = User.find_by(id: params[:id])
+      end
+
+      def user_params
+        params.require(:user).permit(:name, :email, :password)
       end
       
     end
