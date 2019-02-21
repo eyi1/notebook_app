c2b2eca206712326fbae8e066bb1c32c3698544c
github username <github email address>
2019-02-21 12:18:39 -0500

    add create action that renders note json if saved to db

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -5,14 +5,18 @@ module Api::V1
       @notes = Note.all 
       render json: @notes
       end
-      
-      def new 
-      end
 
       def create
+        @note = Note.new(id: params[:id], name: params[:title], params[:content])
+        if @note && @note.save
+          render json: @note
+        else
+          render json: { message: notebook.errors }, status: 400 
+        end
       end
 
       def show
+        render json: @note
       end
 
       def edit
