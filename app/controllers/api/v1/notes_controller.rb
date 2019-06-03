ea04f9288a26720959eb1ba945a3ef751a03c129
github username <github email address>
2019-06-03 14:30:37 -0400

    clean up/refactor code

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -3,21 +3,17 @@ module Api::V1
       before_action :set_note, only: [:show, :update, :destroy]
     
       def index
-      # @notes = Note.all 
-      # render json: @notes
-      if params[:notebook_id]
-        @notes = Notebook.find(params[:notebook_id]).notes     
-        render json: @notes
-      else
-        @notes=Note.all
-        render json: @notes
-      end
-       
+        if params[:notebook_id]
+          @notes = Notebook.find(params[:notebook_id]).notes     
+          render json: @notes
+        else
+          @notes=Note.all
+          render json: @notes
+        end
       end
 
       def create
        @note = Note.new(title: params[:title], content: params[:content], notebook_id: params[:notebook_id])
-       #@note = Note.new(title: params[:title], content: params[:content], notebook_id: 55)
         if @note && @note.save
           render json: @note
         else
