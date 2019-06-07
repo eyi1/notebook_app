100503b77b5e6ce951bc934af06326f7e6fce460
github username <github email address>
2019-06-06 23:19:55 -0400

    add specs for note model

--- app/controllers/api/v1/user_token_controller.rb
+++ app/controllers/api/v1/user_token_controller.rb
@@ -1,2 +1,25 @@
-class UserTokenController < Knock::AuthTokenController
+# class Api::UserTokenController < Knock::AuthTokenController
+# end
+module Api::V1
+    class UserTokenController < Knock::AuthTokenController
+        # def create
+        #     user = User.find_by(email: params[:email])
+        #     byebug
+        #     if user&.authenticate(params[:password])
+        #         render json: {
+        #             jwt: encode_token({id: user.id, name: user.name})
+        #         }
+        #     else
+        #         head :not_found
+        #     end
+        # end
+
+        # private 
+        # def encode_token(payload={})
+        #     exp = 24.hours.from_now
+        #     payload[:exp] = exp.to_i
+        #     JWT.encode(payload, Rails.application.secrets.secret_key_base)
+        # end
+
+    end
 end
---
+++ spec/models/note_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Note, :type => :model do
+    subject { described_class.new(title: "title", notebook_id: "notebook_id") }
+  
+    it "is not valid without a title" do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a content" do
+        subject.content = nil
+        expect(subject).to_not be_valid
+      end
+
+    describe "Associations" do
+        it { should belong_to(:notebook) }
+    end
+
+    # describe "Validations" do
+    #     it { should validate_presence_of(:user) }
+    #   end
+    
+end
