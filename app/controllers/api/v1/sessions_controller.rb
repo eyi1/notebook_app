533aca9a3f658aae3bb2679cac9f4952e803f23e
github username <github email address>
2019-06-11 20:36:36 -0400

    add a sessions controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,27 @@
+module Api::V1
+    class SessionsController < ApplicationController
+        #login
+        def create 
+            @user = User.find_by(email_string: user_params[:email_string])
+
+            if @user && @user.authenticate(user_params[:password])
+                token = generate_token({id: @user.id})
+                response = {
+                    user: UserSerializer.new(@user),
+                    jwt: token
+                  }
+        
+                render json: response   
+            else
+                render json: @user.errors, status: 400
+            end
+            
+        end
+
+        private
+        def user_params
+            params.require(:user).permit(:name, :email_string, :password)
+        end
+
+    end
+end
