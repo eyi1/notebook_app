5482bd73ab36210d3b6ca758be966b11a4c7018e
github username <github email address>
2019-06-12 10:10:26 -0400

    delete commented out line

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  
+
   namespace :api do
     namespace :v1 do
-      #resources :users
       resources :users, :notebooks, :notes
       #post 'user_token' => 'user_token#create'
       #post 'find_user' => 'users#find'
