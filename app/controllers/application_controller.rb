e4cf89051ef481f7958db88664258e77f2ccda60
github username <github email address>
2019-06-12 10:45:31 -0400

    rid of unused line/space

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,24 @@ class ApplicationController < ActionController::API
     def logged_in?
         !!current_user
     end
+
+    def generate_token(payload)
+        JWT.encode payload, ENV["TOKEN_SECRET"], 'HS256' 
+    end
+
+    def decode_token
+        token = request.headers["Authorization"].split(" ")[1]
+        JWT.decode(token, ENV["TOKEN_SECRET"])[0]["id"]
+    end
+
+    def current_user
+        begin
+            User.find_by(id: decode_token)
+        rescue
+            nil
+        end
+        
+     #   User.find_by(id: JWT.decode(request.headers["Authorization"], ENV["TOKEN_SECRET"])[0]["id"])
+        
+    end
 end
